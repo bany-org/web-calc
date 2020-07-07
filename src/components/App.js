de7a10044c1ede29c858273fd6e170ba9008a5dd
jakub_banasiak <soccerkid.bany@gmail.com>
2020-07-07 08:00:16 +0200

    install and test packages

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import { evaluate } from "mathjs";
+
+const Expression = styled.div`
+    background-color: green;
+    font-size: 900;
+    text-align: center;
+`;
 
 function App() {
     return (
         <div className="App">
-            <h1>TEst</h1>
+            <Expression>
+                <h1> test math</h1>
+                <h2>{evaluate("2 * 4 + 3")}</h2>
+            </Expression>
         </div>
     );
 }
