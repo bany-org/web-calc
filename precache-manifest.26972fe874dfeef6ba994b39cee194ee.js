d634fc9be66c3ed579ec143e29f7fea4176a1462
jakub_banasiak <soccerkid.bany@gmail.com>
2020-07-08 10:06:54 +0200

    Updates

---
+++ precache-manifest.26972fe874dfeef6ba994b39cee194ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93807cae98bad419bbd2aef15ef6a6d9",
+    "url": "/web-calc/index.html"
+  },
+  {
+    "revision": "eb3ed1f80f8aab251033",
+    "url": "/web-calc/static/css/main.cba5cd10.chunk.css"
+  },
+  {
+    "revision": "7afccf9e989f1385715a",
+    "url": "/web-calc/static/js/2.95633697.chunk.js"
+  },
+  {
+    "revision": "01d48c74ee01ebacc5c6be7cdbbb2df8",
+    "url": "/web-calc/static/js/2.95633697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3ed1f80f8aab251033",
+    "url": "/web-calc/static/js/main.c3851803.chunk.js"
+  },
+  {
+    "revision": "55726ab8b5973768fb16",
+    "url": "/web-calc/static/js/runtime-main.1187d757.js"
+  }
+]);
